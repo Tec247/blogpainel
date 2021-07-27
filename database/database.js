35960f7e12c25353fcc32a985123cf4e426fbc78
Tec247 <pegazoz99@gmail.com>
2021-07-27 09:21:15 -0300

    Ok

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('blogpainel', 'root', '2280', {
+const connection = new Sequelize('blogpainel', 'root', 'naej', { //sempre mudar a senha naej ou 2280
 
   host: 'localhost',
   dialect: 'mysql'
